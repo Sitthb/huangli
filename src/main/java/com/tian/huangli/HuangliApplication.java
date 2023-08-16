90ca49fd3a819cf3a1ea78e6a2c115c737586b02
sitt <haha@qq.com>
2023-08-16 18:12:18 +0800

    项目初始化

---
+++ src/main/java/com/tian/huangli/HuangliApplication.java
@@ -0,0 +1,13 @@
+package com.tian.huangli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HuangliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuangliApplication.class, args);
+    }
+}
